fea3eed12f734d82cd7a897764bec4af81c355e2
Elizaveta Parada <parada_ev@mail.ru>
2023-08-06 14:42:50 +0200

    ПР13.кр1

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => { res.status(500).send({ message: 'На сервере произошла ошибка' }); });
+app.use('*', (req, res) => { res.status(404).send({ message: 'На сервере произошла ошибка' }); });
 
 app.listen(PORT);
